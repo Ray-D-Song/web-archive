d9cb14c48edfc133e5b88b3ba7b4b4bd3f17bf55
banzhe <banzhe5@outlook.com>
2024-10-14 13:02:19 +0800

    style: optmize folder style when folder name is too long

--- packages/shared/components/folder.tsx
+++ packages/shared/components/folder.tsx
@@ -1,6 +1,6 @@
-import { FileText, Folder as FolderIcon, FolderOpen as FolderOpenIcon } from 'lucide-react'
+import { Folder as FolderIcon, FolderOpen as FolderOpenIcon } from 'lucide-react'
 import { useRef, useState } from 'react'
-import {useDrop} from 'ahooks'
+import { useDrop } from 'ahooks'
 import { Page } from 'types'
 
 interface FolderProps {
@@ -19,23 +19,25 @@ function Folder({ id, name, isOpen, onClick, onDropPage }: FolderProps) {
   const folderRef = useRef(null)
   const [isHover, setIsHover] = useState(false)
   useDrop(folderRef, {
-    onDom: (content) =>{
+    onDom: (content) => {
       setIsHover(false)
       onDropPage?.(content)
     },
-    onDragEnter: () =>{
+    onDragEnter: () => {
       setIsHover(true)
     },
-    onDragLeave: ()=>{
+    onDragLeave: () => {
       setIsHover(false)
     }
   })
 
   return (
     <li ref={folderRef} className={`flex flex-col justify-center cursor-pointer hover:bg-accent w-full p-2 rounded-md ${isOpen || isHover ? 'bg-accent' : ''}`}>
-      <div onClick={handleClick} className="flex items-center">
+      <div onClick={handleClick} className="flex items-center ">
         {isOpen ? <FolderOpenIcon className="w-5 h-5 mr-5 ml-2" /> : <FolderIcon className="w-5 h-5 mr-5 ml-2" />}
-        {name}
+        <div className='flex-1 overflow-hidden text-ellipsis whitespace-nowrap'>
+          {name}
+        </div>
       </div>
     </li>
   )
